689cb3692002c2a6cedf23c47a0fb6f3009fc13c
Paulius <paulius.michelevicius@gmail.com>
2022-02-15 16:07:22 +0000

    IN-93: missing helper function.

--- src/utils/resetChain.js
+++ src/utils/resetChain.js
@@ -1,6 +1,32 @@
 import tokenTypes from './tokenTypes'
 import Api from './vitalamApi'
 import { add } from '../features/tokensSlice'
+import { metadataTypes } from './'
+
+
+const createFormData = (inputs, roles, metadata) => {
+  const formData = new FormData()
+  const outputs = [
+    {
+      roles,
+      metadata: {
+        type: { type: metadataTypes.literal, value: metadata.type },
+        name: { type: metadataTypes.literal, value: metadata.name },
+      },
+      parent_index: 0,
+    },
+  ]
+
+  formData.set(
+    'request',
+    JSON.stringify({
+      inputs,
+      outputs,
+    })
+  )
+
+  return formData
+}
 
 const createRefToken = async (name, dispatch) => {
   const roles = { Owner: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty' } // TODO get correct owner
