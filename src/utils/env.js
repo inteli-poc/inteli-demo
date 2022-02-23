ce78fe990c302379e06e50b7546ba62bf6f35f01
Daniel Cooper <danielcooperxyz@gmail.com>
2022-02-23 13:41:31 +0000

    Export variables individually

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,10 +1,7 @@
-const vars = {
-  AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN || 'inteli.eu.auth0.com',
-  AUTH_CLIENT_ID:
-    process.env.REACT_APP_AUTH_CLIENT_ID || 'BvJaBbxOce4Pwi5PZpjBTStvNWwzugPd',
-  AUTH_AUDIENCE: process.env.REACT_APP_AUTH_AUDIENCE || 'inteli-dev',
-  API_HOST: process.env.REACT_APP_API_HOST || 'localhost',
-  API_PORT: process.env.REACT_APP_API_PORT || 3001,
-}
-
-export default { ...vars }
+export const AUTH_DOMAIN =
+  process.env.REACT_APP_AUTH_DOMAIN || 'inteli.eu.auth0.com'
+export const AUTH_CLIENT_ID =
+  process.env.REACT_APP_AUTH_CLIENT_ID || 'BvJaBbxOce4Pwi5PZpjBTStvNWwzugPd'
+export const AUTH_AUDIENCE = process.env.REACT_APP_AUTH_AUDIENCE || 'inteli-dev'
+export const API_HOST = process.env.REACT_APP_API_HOST || 'localhost'
+export const API_PORT = process.env.REACT_APP_API_PORT || 3001
