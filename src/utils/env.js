60d1741e4541e0542ec2b7580c4d6d420e319223
Daniel Cooper <danielcooperxyz@gmail.com>
2022-02-23 14:23:04 +0000

    fix default dev port

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,4 +4,4 @@ export const AUTH_CLIENT_ID =
   process.env.REACT_APP_AUTH_CLIENT_ID || 'BvJaBbxOce4Pwi5PZpjBTStvNWwzugPd'
 export const AUTH_AUDIENCE = process.env.REACT_APP_AUTH_AUDIENCE || 'inteli-dev'
 export const API_HOST = process.env.REACT_APP_API_HOST || 'localhost'
-export const API_PORT = process.env.REACT_APP_API_PORT || 3001
+export const API_PORT = process.env.REACT_APP_API_PORT || 3000
