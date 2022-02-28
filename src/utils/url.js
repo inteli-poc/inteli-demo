044da22a04b5b8030c9b3c1f5352ea94014ed18c
Daniel Cooper <danielcooperxyz@gmail.com>
2022-02-28 14:38:00 +0000

    Use a single declaration statement

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,5 @@
 export const getCurrentBaseUrl = () => {
-  const protocol = window.location.protocol
-  const domain = window.location.hostname
-  const port = window.location.port
+  const { protocol, hostname, port } = window.location
 
-  return `${protocol}//${domain}:${port ? port : ''}`
+  return `${protocol}//${hostname}:${port ? port : ''}`
 }
